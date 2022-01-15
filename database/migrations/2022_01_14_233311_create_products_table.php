54c762abcb63da84599f82fa22a44be919af0f8d
murage <murage@labskenya.com>
2022-01-15 18:30:44 +0300

    refactor: renamed user products to products

--- database/migrations/2022_01_14_233311_create_products_table.php
+++ database/migrations/2022_01_14_233311_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserProductsTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUserProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_products', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             https://laravel.com/docs/8.x/migrations#column-method-foreignId            $table->id();
             $table->string('name')->nullable(false);
             $table->text('description');
@@ -36,6 +36,6 @@ class CreateUserProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_products');
+        Schema::dropIfExists('products');
     }
 }
