970f82cc5bc3d912d9e8de52778906fe22652739
murage <murage@labskenya.com>
2022-01-16 13:25:37 +0300

    fix: force all responses without accept headers to be json

--- app/Http/Middleware/ExpectJson.php
+++ app/Http/Middleware/ExpectJson.php
@@ -4,17 +4,20 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class ExampleMiddleware
+class ExpectJson
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
+        if (!$request->hasHeader('accepts')) {
+            $request->headers->set('Accept', 'application/json');
+        }
         return $next($request);
     }
 }
