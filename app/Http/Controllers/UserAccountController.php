a97688fb576b3d2106423995261a2c1dd608e84a
murage <murage@labskenya.com>
2022-01-16 20:31:19 +0300

    feat: add endpoint to update user details

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 
 class UserAccountController extends Controller
 {
@@ -81,4 +82,48 @@ class UserAccountController extends Controller
                 'user' => $user], 201);
 
     }
+
+
+    public function update(Request $request, $id)
+    {
+        //retrieve the user
+        $user = User::findOrFail($id);
+
+        // Validate the request
+        $validated = $this->validate($request, [
+            'first_name' => ['sometimes', 'min:2', 'max:200'],
+            'last_name' => ['sometimes', 'max:200'],
+            'phone_number' => ['sometimes', 'digits_between:10,12'],
+            'email' => ['sometimes', 'email', 'max:100',
+                Rule::unique('users', 'email')->ignore($user)],
+            'role_id' => ['sometimes', 'exists:roles,id']
+        ],
+            ['phone_number.digits_between' => 'Format should be either 2547XXXXXXXX or 07XXXXXXXX',
+                'role_id.exists' => 'Role assigned does not exists']);
+
+        DB::beginTransaction();
+
+        unset($validated['role_id']);
+
+        $validated['user_id'] = Auth::id(); #attach the editor
+
+        #do the actual update
+        $user->update($validated);
+
+        if ($request->has('role_id')) {
+            //if the roles have been update, sync
+            $user->roles()->sync(['id' => $request->only('role_id')],
+                ['created_at' => Carbon::now(), 'updated_at' => Carbon::now()]);
+        }
+
+        DB::commit();
+
+
+        //reload the roles
+        $user->load('roles');
+        return response()
+            ->json(['message' => 'User details updated successfully.',
+                'user' => $user], 201);
+    }
+
 }
